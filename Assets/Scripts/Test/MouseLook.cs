309de18cffe95a1a08b1bd30f6df10329a943a7d
qoxowjd <qoxowjd77@naver.com>
2024-02-16 15:18:03 +0900

    맵 프리팹 만들기
    지형 맵 프리펨 설정

--- Assets/Scripts/Test/MouseLook.cs
+++ Assets/Scripts/Test/MouseLook.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // https://acredev.tistory.com/18
 }
