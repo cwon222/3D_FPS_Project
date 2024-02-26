f9c13f10773966d17b3a1ec98d6d73bdeab9bc9d
cwon222 <qoxowjd77@naver.com>
2024-02-26 22:13:09 +0900

    적에게 피격당햇을 때 UI변경
    적에게 피격당햇을 때 UI 변경하는 스크립트

--- Assets/Scripts/FirstPersonFPS/Player/Status.cs
+++ Assets/Scripts/FirstPersonFPS/Player/Status.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
+/// 체력 정보가 바뀔 때마다 외부에 있는 메소드 자동 호출할 수 있게 이벤트 클래스 생성
+/// </summary>
+[System.Serializable]
+public class HPEvent : UnityEngine.Events.UnityEvent<int, int> { }
+
+/// <summary>
 /// 캐릭터의 상태를 관리하는 스크립트
 /// </summary>
 public class Status : MonoBehaviour
 {
+    /// <summary>
+    /// HP이벤트 클래스 인스턴스 선언
+    /// </summary>
+    [HideInInspector]
+
+    [Header("Move Speed")]
+    public HPEvent onHPEvent = new HPEvent();
     /// <summary>
     /// 걷는 속도
     /// </summary>
@@ -19,6 +32,18 @@ public class Status : MonoBehaviour
     [SerializeField]
     float runSpeed;
 
+    [Header("HP")]
+    /// <summary>
+    /// 최대 체력
+    /// </summary>
+    [SerializeField]
+    int maxHP = 100;
+
+    /// <summary>
+    /// 현재 체력
+    /// </summary>
+    int currentHP;
+
     /// <summary>
     /// 외부에서 걷는 속도를 확인하기 위한 프로퍼티
     /// </summary>
@@ -28,4 +53,41 @@ public class Status : MonoBehaviour
     /// 외부에서 뛰는 속도를 확인하기 위한 프로퍼티
     /// </summary>
     public float RunSpeed => runSpeed;
+
+    /// <summary>
+    /// 현재 체력 HP 확인을 위한 프로퍼티
+    /// </summary>
+    public int CurrentHP => currentHP;
+
+    /// <summary>
+    /// 최대 체력 HP 확인을 위하나 프로퍼티
+    /// </summary>
+    public int MaxHP => maxHP;
+
+    private void Awake()
+    {
+        currentHP = maxHP; // 현재 체력을 최대 체력으로 설정
+    }
+
+    /// <summary>
+    /// 데미지 감소 확인용 변수
+    /// </summary>
+    /// <param name="damage">데미지</param>
+    /// <returns></returns>
+    public bool DecreaseHP(int damage)
+    {
+        int preHP = currentHP; // 이전의 현재 체력 저장
+
+        // 현재 체력에서 데미지를 뺐을 때 0보다 크면 현재 체력에 데미지 만큼 감소한 체력을 저장 0보다 작으면 현재 체력에 0을 저장
+        currentHP = currentHP - damage > 0 ? currentHP - damage : 0;
+
+        onHPEvent.Invoke(preHP, currentHP); // 체력이 바뀐것을 알리기
+
+        if (currentHP == 0) // 현재 체력이 0이면 
+        {
+            return true; // 참
+        }
+        // 현재 체력이 0이 아니면
+        return false;   // 거짓
+    }
 }
--- Assets/Scripts/FirstPersonFPS/UI/PlayerHUD.cs
+++ Assets/Scripts/FirstPersonFPS/UI/PlayerHUD.cs
@@ -1,4 +1,3 @@
-using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -13,6 +12,12 @@ public class PlayerHUD : MonoBehaviour
     /// </summary>
     [SerializeField]
     Weapon weapon;
+
+    /// <summary>
+    /// 플레이어의 상태(이동속도, 체력)
+    /// </summary>
+    [SerializeField]
+    Status status;
 
     [Header("Weapon Base")]
     /// <summary>
@@ -57,6 +62,25 @@ public class PlayerHUD : MonoBehaviour
     /// </summary>
     List<GameObject> magazineList;
 
+    [Header("HP & BloodScreen UI")]
+    /// <summary>
+    /// 플레이어의 체력을 출력하기 위한
+    /// </summary>
+    [SerializeField]
+    TextMeshProUGUI textHP;
+
+    /// <summary>
+    /// 플레이어가 공격 받으면 화면에 표시되기 위한
+    /// </summary>
+    [SerializeField]
+    Image imageBloodScreen;
+
+    /// <summary>
+    /// 공격 받을때 변경되는 이미지를 설정하기 위한
+    /// </summary>
+    [SerializeField]
+    AnimationCurve curveBloodScreen;
+
     private void Awake()
     {
         SetupWeapon();  // 현재 무기 정보 갱신 함수 호출
@@ -69,6 +93,7 @@ public class PlayerHUD : MonoBehaviour
         // Weapon::OnEnable() : 무기 오브젝트가 활성화 될 때
         // Weapon::OnAttack() : 공격으로 탄이 소모되었을 떄 
         //UpdateAmmoHUD() 호출한다
+        status.onHPEvent.AddListener(UpdateHPHUD);
     }
 
     /// <summary>
@@ -90,6 +115,9 @@ public class PlayerHUD : MonoBehaviour
         textAmmo.text = $"<size=40>{currentAmmo}/</size>{maxAmmo}"; // 텍스트 갱신
     }
 
+    /// <summary>
+    /// 화면에 탄창 이미지 아이콘을 관리 함수
+    /// </summary>
     private void SetupMagazine()
     {
         // weapon에 등록되어있는 최대 탄창 개수만큼 Image Icon을 생성
@@ -110,6 +138,10 @@ public class PlayerHUD : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 탄창 이미지를 갱신할 함수
+    /// </summary>
+    /// <param name="currentMagazine">현재 탄창 수</param>
     private void UpdateMagazineHUD(int currentMagazine)
     {
         // 전부 비활성화
@@ -121,6 +153,42 @@ public class PlayerHUD : MonoBehaviour
         for(int i = 0; i < currentMagazine; ++i)
         {
             magazineList[i].SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// 바뀐 체력을 업데이트할 함수
+    /// </summary>
+    /// <param name="previous">이전 체력</param>
+    /// <param name="current">현재 체력</param>
+    private void UpdateHPHUD(int previous, int current)
+    {
+        textHP.text = "HP" + current; // 체력 텍스트 변경
+
+        if(previous - current > 0) // 남은 체력이 0 이상이면
+        {
+            StopCoroutine("OnBloodScreen"); // 피격 이미지 코루틴 정지
+            StartCoroutine("OnBloodScreen");    // 피격 이미지 코루틴 실행
+        }
+    }
+
+    /// <summary>
+    /// 플레이어가 피격당하면 이미지를 관리할 코루틴
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator OnBloodScreen()
+    {
+        float percent = 0;
+
+        while(percent < 1)
+        {
+            percent += Time.deltaTime;
+
+            Color color = imageBloodScreen.color;
+            color.a = Mathf.Lerp(1, 0, curveBloodScreen.Evaluate(percent)); // 알파 값을 1에서 0까지 1초 동안 감ㅅ소
+            imageBloodScreen.color = color;
+
+            yield return null;
         }
     }
 }
