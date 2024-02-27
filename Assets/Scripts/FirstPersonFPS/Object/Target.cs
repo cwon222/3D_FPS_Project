6c058f7e306f57522a6c3912d22843b50d83fc5b
cwon222 <qoxowjd77@naver.com>
2024-02-27 22:48:26 +0900

    과녁 오브젝트 만들기
    과녁을 쏴서 맞추면 과녁이 넘어지고 설정한 시간뒤에 다시 일어난다

---
+++ Assets/Scripts/FirstPersonFPS/Object/Target.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : InteractionObject
+{
+    [SerializeField]
+    float targetUpDelayTime = 3.0f;
+
+    bool isPossibleHit = true;
+
+    public override void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+
+        if(currentHP < 0 && isPossibleHit == true)
+        {
+            isPossibleHit = false;
+
+            StartCoroutine("OnTargetDown");
+        }
+    }
+
+    IEnumerator OnTargetDown()
+    {
+        yield return StartCoroutine(OnAimation(0, 90));
+
+        StartCoroutine("OnTargetUp");
+    }
+
+    IEnumerator OnTargetUp()
+    {
+        yield return new WaitForSeconds(targetUpDelayTime);
+
+        yield return StartCoroutine(OnAimation(90, 0));
+    }
+
+    IEnumerator OnAimation(float start, float end)
+    {
+        float percent = 0.0f;
+        float current = 0.0f;
+        float time = 1.0f;
+
+        while(percent < 1)
+        {
+            current += Time.deltaTime;
+            percent = current / time;
+
+            transform.rotation = Quaternion.Slerp(Quaternion.Euler(start, 0, 0), Quaternion.Euler(end, 0, 0), percent);
+
+            yield return null;
+        }
+    }
+}
